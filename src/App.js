4695cb0dc1b4eabbd2b3b54cf31b7dae8e00b071
R B <rons9n@gmail.com>
2021-02-01 18:43:49 -0500

    added first styling, dependencies and user/chatroom

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine'
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="5320b691-dc8c-490c-87e0-10e0446090f4"
+            userName="RonB"
+            userSecret="123"
+            />
+    )
+}
+
+export default App;
